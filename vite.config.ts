b2ff4a8ed40951f8052160fb1c507d1233b3553a
KiwiUserSFTW <danielkostenko0305@gmail.com>
2025-04-09 14:07:47 +0200

    refactor scss replaced deprecated @ipmort with @use

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@styles/variables";`,
+        additionalData: `@use "@styles/variables" as *;`,
       },
     },
   },
